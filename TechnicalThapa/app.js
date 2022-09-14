84084eee750c009e50d954ff894bf014af21d8e9
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-09-14 20:46:01 +0530

    .

--- TechnicalThapa/app.js
+++ TechnicalThapa/app.js
@@ -1,1 +1,16 @@
-require('fs')
+const fs = require("fs")
+
+fs.mkdirSync("test")
+
+fs.writeFileSync("./test/test.js", "Test successful")
+
+fs.appendFileSync("./test/test.js", "   Voilla....DOne !!!")
+
+const readData = fs.readFileSync("./test/test.js", "utf8")
+console.log(readData)
+
+//to delete file
+// fs.unlinkSync("./test/test.js")
+
+//to delete folder
+// fs.rmdirSync("test")
