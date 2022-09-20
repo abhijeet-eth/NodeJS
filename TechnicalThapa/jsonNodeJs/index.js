053d4e3e5da8ef0d59e0e595445afe20fd99c55b
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-09-20 08:51:25 +0530

    JSON

--- TechnicalThapa/jsonNodeJs/index.js
+++ TechnicalThapa/jsonNodeJs/index.js
@@ -1,1 +1,12 @@
-a
+const bioData = {
+    name: "ABhijeet",
+    age: 26,
+    city: "Ranchi"
+
+};
+
+const jsonData = JSON.stringify(bioData)
+console.log(jsonData)
+
+const objData = JSON.parse(jsonData)
+console.log(objData)
