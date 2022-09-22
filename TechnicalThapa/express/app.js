d01e8ce257be6b88d726acf72e0154c77afa8240
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-09-22 09:36:28 +0530

    express

---
+++ TechnicalThapa/express/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const app = express()
+
+//The callback function has two parameters, request(req) and response(res).
+//The request object(req) represents the HTTP request and
+// has properties for the request query string, parameters, body
+// HTTP headers, etc
+
+//Similarly, the response object represents the HTTP response
+//that the Express app sends when it receives an HTTP request
+
+app.get("/", (req, res) => {
+    res.send("hello from the express")
+})
+
+app.get("/about", (req, res) => {
+    res.send("hello from the about page")
+})
+
+app.listen(8000, () => {
+    console.log("listening to port 8000")
+})
+
