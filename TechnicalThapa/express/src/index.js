4fe69b80f6cc2bbfb05f95b8708f4b962f7b3242
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-09-26 17:57:05 +0530

    more chages

--- TechnicalThapa/express/src/index.js
+++ TechnicalThapa/express/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-
+const requests = require('requests')
 //built-in middleware
 console.log(path.join(__dirname, "../public"))
 const staticPath = path.join(__dirname, "../public")
@@ -22,6 +22,28 @@ app.get("/", (req, res) => {
     res.send("hello from our server")
 })
 
+app.get("/about", (req, res) => {
+    requests(
+        `https://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=c9e3d9265c402cc4d54d66bdd0b3bec7`
+    )
+        .on("data", (chunk) => {
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+            console.log(`city name is ${arrData[0].name} and the temp is ${arrData[0].main.temp}`)
+
+            res.write(arrData[0].name)
+        })
+        .on("end", (err) => {
+            if (err) return console.log("Connection closed due to errors", err);
+            res.end()
+        })
+})
+
+app.get("/about/*", (req, res) => {
+    res.render("404", {
+        errorComment: "Ooops this page could't reload"
+    })
+})
 app.listen(8000, () => {
     console.log("listening to port 8000")
 })
