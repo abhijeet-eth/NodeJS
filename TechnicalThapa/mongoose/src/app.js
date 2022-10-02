de41b7613abe31c491763f56f634fc5fbfdd305b
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-10-02 14:42:35 +0530

    mongoose

--- TechnicalThapa/mongoose/src/app.js
+++ TechnicalThapa/mongoose/src/app.js
@@ -1,1 +1,36 @@
 const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/abhijeetDatabase")
+    .then(console.log("Connection is successful"))
+    .catch((err) => console.log(err))
+
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Playlist = new mongoose.model("Playlist", playlistSchema)
+
+//create a document or insert
+
+const reactPlaylist = new Playlist({
+    name: "React JS",
+    ctype: "Front End",
+    videos: 10,
+    author: "ABhijeet Sinha",
+    active: true,
+
+})
+
+reactPlaylist.save()
