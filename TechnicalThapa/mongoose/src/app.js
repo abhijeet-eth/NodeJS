9195d58454be470dcdf05153c02a67bf0715d78e
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-10-04 11:06:48 +0530

    mongoose

--- TechnicalThapa/mongoose/src/app.js
+++ TechnicalThapa/mongoose/src/app.js
@@ -18,19 +18,63 @@ const playlistSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+
 })
 
 const Playlist = new mongoose.model("Playlist", playlistSchema)
 
 //create a document or insert
+const createDocument = async () => {
+    try {
+        const reactPlaylist = new Playlist({
+            name: "React JS",
+            ctype: "Front End",
+            videos: 10,
+            author: "ABhijeet Sinha",
+            active: true
+        })
 
-const reactPlaylist = new Playlist({
-    name: "React JS",
-    ctype: "Front End",
-    videos: 10,
-    author: "ABhijeet Sinha",
-    active: true,
+        const mongoosePlaylist = new Playlist({
+            name: "Mongoose",
+            ctype: "Database",
+            videos: 32,
+            author: "Yoyo",
+            active: true
+        })
 
-})
+        const jsPlaylist = new Playlist({
+            name: " JS",
+            ctype: "Front End",
+            videos: 100,
+            author: " Sinha",
+            active: true
+        })
 
-reactPlaylist.save()
+        const mongoPlaylist = new Playlist({
+            name: "Mongo",
+            ctype: "Database",
+            videos: 40,
+            author: "Lolo",
+            active: true
+        })
+
+        const result = await Playlist.insertMany([reactPlaylist, mongoosePlaylist, jsPlaylist, mongoPlaylist])
+        console.log(result)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+// createDocument()
+
+//How to read / query documents
+
+const getDocument = async () => {
+    const result = await Playlist.find({ ctype: "Database" })
+    console.log(result)
+
+}
+
+getDocument()
