9a8dc44f953175a8ac5addbdc77b6488815c2f4c
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-10-06 10:37:53 +0530

    restapi Students

--- TechnicalThapa/restfulapi/src/app.js
+++ TechnicalThapa/restfulapi/src/app.js
@@ -15,15 +15,61 @@ app.use(express.json());
 
 //create  a new student
 
-app.post("/students", (req, res) => {
-    console.log(req.body)
-    const user = new Student(req.body)
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+// *** using then method *** //
+// app.post("/students", (req, res) => {
+//     console.log(req.body)
+//     const user = new Student(req.body)
+//     user.save().then(() => {
+//         res.status(201).send(user)
+//     }).catch((e) => {
+//         res.status(400).send(e);
+//     })
+// })
+
+// *** using async await method *** //
+
+app.post("/students", async (req, res) => {
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser)
+    } catch (e) {
+
+        res.status(400).send(e)
+    }
 })
+
+//read the Data of registered students
+app.get("/students", async (req, res) => {
+    try {
+        const studentsData = await Student.find()
+        res.send(studentsData)
+    } catch (e) {
+        res.send(e)
+
+    }
+})
+
+//read the Data of a specific student(schema) using Id
+app.get("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const studentData = await Student.findById(_id)
+        if (!studentData) {
+            return res.status(404).send()
+        }
+        else {
+            return res.status(200).send(studentData)
+        }
+
+    } catch (e) {
+        res.status(500).send(e)
+
+    }
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Connect is successful at ${port}`)
