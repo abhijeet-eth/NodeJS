62f173d2a63df9a6fbb4fd8d3c10ada09869a067
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-10-13 12:25:56 +0530

    bcrypt

--- TechnicalThapa/MyProject/src/models/signup.js
+++ TechnicalThapa/MyProject/src/models/signup.js
@@ -1,27 +1,42 @@
 const Mongoose = require("mongoose")
+const bcrypt = require("bcryptjs");
 
 const signupSchema = new Mongoose.Schema({
-    firstName:{
+    firstName: {
         type: String,
         required: true
     },
-    lastName:{
+    lastName: {
         type: String,
         required: true
     },
-    email:{
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
         type: String,
         required: true
     },
-    password:{
-        type: String,
-        required: true
-    },
-    confirmPassword:{
+    confirmPassword: {
         type: String,
         required: true
     }
 })
 
-const SignUpPage = Mongoose.model("SignUpPage",signupSchema );
+//acting as middleware. In home.js, "/signup" before saving it will first hash the value
+//of password and hashed value will get save in the DB  
+signupSchema.pre("save", async function (next) {
+    if (this.isModified("password")) { //only when 'password' field is modified then continue further
+        console.log("I am middleware")
+        this.password = await bcrypt.hash(this.password, 10) //'this' refers to schema model
+
+        this.confirmPassword = undefined; //this will not print confirmPassword in database
+
+    }
+    next();
+})
+
+const SignUpPage = Mongoose.model("SignUpPage", signupSchema);
 module.exports = SignUpPage;
--- TechnicalThapa/MyProject/src/routes/home.js
+++ TechnicalThapa/MyProject/src/routes/home.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 require("../db/conn")
 const SignUpPage = require("../models/signup")
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
     res.send("welcome to my home page")
@@ -16,6 +17,7 @@ router.post("/signup", async (req, res) => {
         if (password === cpassword) {
             const doingSignUp = new SignUpPage(req.body)
             console.log(doingSignUp)
+
             const insertSignup = await doingSignUp.save()
             res.status(200).send(insertSignup)
         }
@@ -28,6 +30,40 @@ router.post("/signup", async (req, res) => {
         res.status(400).send(error)
     }
 })
+
+router.post("/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const userEmail = await SignUpPage.findOne({ email })
+
+        //bcrypt.compare(a,b) a: password entered (without hashed) ; b = hashed password saved in DB
+        const isMatch = await bcrypt.compare(password, userEmail.password)
+
+        console.log(userEmail)
+        if (isMatch) {
+            res.status(201).send("Password matched")
+        } else {
+            res.send("Password not matched")
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// ******** How to hash and compare password using BCrypt ********** //
+
+// const securePassword = async (pwd) => {
+//     //creating hash of password
+//     const passwordHash = await bcrypt.hash(pwd, 10) //10 is the number of rounds for hashing and excrypting the variable pwd
+//     console.log(passwordHash)
+
+//     //to match password 
+//     const passwordmatch = await bcrypt.compare(pwd, passwordHash) //return boolean
+//     console.log(passwordmatch)
+// }
+// securePassword("abhij")
+
 
 module.exports = router
 
