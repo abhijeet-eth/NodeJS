529f6e0213a6878fe2d0d31f8bb39f2d2fc76d22
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-10-23 12:05:49 +0530

    TechnicalThapa => Middleware

---
+++ TechnicalThapa/Middleware/server/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+const middleware = (req, res, next) => {
+    console.log("I am Middleware")
+    next();
+}
+
+app.get("/", (req, res) => {
+    res.send("Home Connected")
+})
+
+app.get("/about", middleware, (req, res) => {
+    res.send("About Connected")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Connected")
+})
+
+app.get("/signin", (req, res) => {
+    res.send("Signin Connected")
+})
+
+app.listen(3000, () => {
+    console.log("App connected to port 3000")
+})
